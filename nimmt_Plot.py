b4ddfe4340160a465fb296f108702738093efd09
MasaYan24 <m.takahashi.public+github@gmail.com>
2018-07-24 17:49:17 +0900

    nimmt_plot.py complete

--- nimmt_Plot.py
+++ nimmt_Plot.py
@@ -1,88 +1,63 @@
 #!/usr/local/bin/python3
 
-# NUM_GAME (下に定義) の回数一気に python で実行するスクリプト
-#pandasを用いてcsvを解析し，平均順位，1位を取った割合，各々の順位を取った回数を計算する
-#デフォルトではstat.csvにデータを出力する（既にある場合は消される）
-#matplotlib,numpyを用いて棒グラフで視覚化する
-
-import random
-import os
-import sys
-import copy
-import csv
-import pandas as pd
-from pandas import DataFrame,Series
+import numpy
+import pandas
+import matplotlib
 import matplotlib.pyplot as plt
-import numpy as np
+import sys
 
+class ReadPlot(object):
+    def __init__(self,datafile="stat.csv",figfile="stat.png"):
+        self.__datafile = datafile
+        self.__figfile = figfile
+        self.__readfile(datafile=datafile)
 
-dataset = pd.read_csv(output,header=0,encoding='utf-8')#make dataframe
-f=open(output,"a")
-f.write("average:,")
-average=list(range(len(players_list)))#initial
-for i in range(len(players_list)-1):
-    data_list=dataset.iloc[0:NUM_GAME+1,i+1].values.tolist()#CSVの列をリストで取り出す
-    ave=sum(data_list) / len(data_list)
-    average[i]=ave
-    avest=str(ave)
-    f.write(avest+",")
-data_list=dataset.iloc[0:NUM_GAME+1,len(players_list)].values.tolist()#端にカンマを入れさせない
-ave=sum(data_list) / len(data_list)
-average[len(players_list)-1]=ave
-avest=str(ave)
-f.write(avest)
-f.write('\n')
+    def __readfile(self,datafile="stat.csv"):
+        self.__data = pandas.read_csv(datafile,header=0,encoding='utf-8',skipinitialspace=True)
+        self.__players_class_list = list(self.__data.columns)
+        self.__num_players = len(self.__players_class_list)
+        self.__players_name_list = [name.replace("AI","") for name in self.__players_class_list]
+        # calculate statistic values
+        self.__average_list = list(self.__data.mean())
+        self.__ranking = sorted(range(self.__num_players),key=lambda k:self.__average_list[k])
+        self.__std_list = list(self.__data.std(ddof=False))
+        self.__num_games = len(self.__data)
 
+    @property
+    def data(self):
+        return self.__data
 
-for k in range(len(players_list)):
-    ss=str(k)
-    f.write("num_"+ ss + ":, ")
-    for i in range(len(players_list)-1):
-        data_list=dataset.iloc[0:NUM_GAME+1,i+1].values.tolist()#CSVの列をリストで取り出す
-        count=data_list.count(k)
-        ct=str(count)
-        f.write(ct)
-        f.write(",")
-    data_list=dataset.iloc[0:NUM_GAME+1,len(players_list)].values.tolist()#端にカンマを入れさせない
-    count=data_list.count(k)
-    ct=str(count)
-    f.write(ct)
-    f.write('\n')
-f.close()
-
-ct1st=list(range(len(players_list)))#initial
-for i in range(len(players_list)):
-    data_list=dataset.iloc[0:NUM_GAME+1,i+1].values.tolist()#CSVの列をリストで取り出す
-    ct1st[i]=data_list.count(0)
-
-
-df = pd.read_csv(output,header=0,encoding='utf-8')
-color=['red','skyblue','springgreen','forestgreen','limegreen','lightgreen','mediumspringgreen','greenyellow','black']
-w = 0.4
-X = range(len(players_list)) 
-Y=list(range(len(players_list)))#initial
-Y[0]=df.iloc[NUM_GAME+1,1:len(players_list)+1].values.tolist()
-plt.bar(X, Y[0], color=color[0], width=w,label=0, align="center")
-bt=np.array([0.]*len(players_list))
-for i in range(len(players_list)-1):
-	bt+=np.array(Y[i])
-	Y[i+1]=df.iloc[NUM_GAME+i+2,1:len(players_list)+1].values.tolist()
-	plt.bar(X, Y[i+1], color=color[i+1],bottom=bt, width=w,label=i+1, align="center")
-
-plt.legend(bbox_to_anchor=(1.1,0.6))
-name=[]
-for i in range(len(players_list)):
-    name.append(str(players_list[i].__class__.__name__))
-plt.xticks(X,name,fontsize=7)
-
-for i in range(len(players_list)):
-    plt.text(i, 0.1*NUM_GAME, average[i], ha='center', va='bottom')
-
-
-for i in range(len(players_list)):
-    st=str(100*ct1st[i]/NUM_GAME)
-    plt.text(i, 0.05*NUM_GAME, st+"%", ha='center', va='center')
-
-plt.show()
-
+    def plot(self):
+        fig, ax = plt.subplots() # create plot space and axis
+        plt.rcParams["font.size"] = 7 # font size
+        fig.subplots_adjust(bottom=0.2) # modify bottom margin
+        plt.ylim([0,self.__num_games]) # y-range
+        _xticks = [i for i in range(self.__num_players)] # x-tics = [0, 1, 2, ...]
+        _xlabel = [self.__players_name_list[i] for i in self.__ranking] # x-label (list of names)
+        _bottom = [0 for i in range(self.__num_players)] # [0,0,0,...]
+        for ith in range(self.__num_players): # loop for ith-rank: from 1st, 2nd, 3rd, ...
+            _list = []
+            for irank in self.__ranking: # loop for each players: from top player to the last
+                _player = self.__players_class_list[irank] # get column name of irank-th player
+                _count_rank = (self.__data[_player]==ith).sum() # get column data which is ith and count
+                _list.append(_count_rank) # append to _list
+            # plot bar-graph.
+            # x-position, y-height, x-label, offset, align, label for legend
+            ax.bar(_xticks, _list, tick_label=_xlabel, bottom=_bottom, align="center", label=str(ith))
+            for j in range(len(_list)):
+                if _list[j] >= self.__num_games*0.04: # if there is a space in bin
+                    # print number of ith-game
+                    ax.annotate(_list[j], (_xticks[j],_bottom[j]+_list[j]/2),ha='center',va='center',color='white')
+            _bottom = [_bottom[i]+_list[i] for i in range(self.__num_players)] # update offset
+        ax.tick_params(axis='x',which='both',bottom=False,top=False,labelbottom=True) # delete x-ticks
+        ax.text(-1,-self.__num_games*0.1,'ave.',ha='center',va='center') # print 'ave.'
+        ax.text(-1,-self.__num_games*0.15,'std.',ha='center',va='center') # print 'std.'
+        # print ave. and std. for each player
+        for i in range(self.__num_players):
+            ax.text(_xticks[i],-self.__num_games*0.11,self.__average_list[self.__ranking[i]],ha='center',va='center')
+            ax.text(_xticks[i],-self.__num_games*0.16,'{:.2f}'.format(self.__std_list[self.__ranking[i]]),ha='center',va='center')
+        # set legend format
+        ax.legend(bbox_to_anchor=(0.,1.,1.,0.05),ncol=self.__num_players,borderaxespad=0.,mode='expand',edgecolor='None')
+        # save as png
+        plt.savefig(self.__figfile)
 
